16c229cd71832d687b266b6c24728d750c7eb611
sibi404 <sibikottukandiyil2@gmail.com>
2022-12-20 20:01:20 +0530

    In cycle7 bubble sorting added

---
+++ cycle7/BubbleSort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int size;
+void bubbleSort(int ar[]){
+	for(int i = 0;i < size-1;i++){
+		for(int j=0;j<size-i-1;j++ ){
+			if (ar[j] < ar[j + 1]){
+				int temp = ar[j];
+				ar[j] = ar[j + 1];
+				ar[j + 1] = temp;
+			}
+		}
+	}
+}
+void printList(int ar[]){
+	for (int i = 0; i < size; i++){
+		printf("%d\t",ar[i]);
+	}
+	printf("\n");
+}
+int main(){
+	printf("Enter no.of elements : ");
+	scanf("%d",&size);
+	int ar[size];
+	printf("Enter the elements\n");
+	for (int i = 0; i < size; i++){
+		scanf("%d",&ar[i]);
+	}
+	printf("List before sorting\n");
+	printList(ar);
+	bubbleSort(ar);
+	printf("List after sorting\n");
+	printList(ar);
+	return 0;
+}
