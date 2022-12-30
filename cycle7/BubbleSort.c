97b4f8a0cf620a8f144eeba5ac8d498302d8cdc0
sibi404 <sibikottukandiyil2@gmail.com>
2022-12-30 21:05:25 +0530

    in cycle7 BubbleSort edited

--- cycle7/BubbleSort.c
+++ cycle7/BubbleSort.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
 int size;
-void bubbleSort(int ar[]){
-	for(int i = 0;i < size-1;i++){
-		for(int j=0;j<size-i-1;j++ ){
-			if (ar[j] < ar[j + 1]){
-				int temp = ar[j];
-				ar[j] = ar[j + 1];
-				ar[j + 1] = temp;
-			}
-		}
-	}
-}
+
+
 void printList(int ar[]){
 	for (int i = 0; i < size; i++){
 		printf("%d\t",ar[i]);
 	}
 	printf("\n");
 }
+
+void bubbleSort(int ar[]){
+	for(int i = 0;i < size-1;i++){
+		for(int j=0;j<size-i-1;j++ ){
+			if (ar[j] > ar[j + 1]){
+				int temp = ar[j];
+				ar[j] = ar[j + 1];
+				ar[j + 1] = temp;
+			}
+		}
+		printf("Iteration %d\n",i);
+		printList(ar);
+	}
+}
+
 int main(){
 	printf("Enter no.of elements : ");
 	scanf("%d",&size);
