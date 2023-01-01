d9644b83ffae406440c1d6e9bc780a05c6a12f7e
sibi404 <sibikottukandiyil2@gmail.com>
2023-01-01 10:52:53 +0530

    In cycle3 Infix to postfix added

---
+++ cycle3/InfixToPostfix.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#define MAX_SIZE 20
+char expr[MAX_SIZE],result[MAX_SIZE],stack[MAX_SIZE];
+int top = -1;
+
+
+int operatiorPrecedence(char a){
+	if (a == '+' || a == '-'){
+		return 1;
+	}else if (a == '*' || a == '/'){
+		return 2;
+	}else if(a == '^'){
+		return 3;
+	}else{
+		return 0;
+	}
+}
+
+int isAlpha(char a){
+	if ((a >= 'a' && a <= 'z') || a >= 'A' && a <= 'Z'){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+void convertInfixPostfix(char ar[]){
+	int j = 0;
+	for (int i = 0; ar[i] != '\0'; i++){
+		// printf("operator : %c Precedence %d\n",ar[i],operatiorPrecedence(ar[i]));
+		if (isAlpha(ar[i])){
+			result[j++] = ar[i];
+		}else if (ar[i] == '('){
+			stack[++top] = ar[i];
+		}else if(ar[i] == ')'){
+			while(stack[top] != '('){
+				result[j++] = stack[top--];
+			}
+			top--;
+		}else{
+			if((top == -1) || (stack[top] == '(') || (operatiorPrecedence(ar[i]) > operatiorPrecedence(stack[top]))){
+				stack[++top] = ar[i];
+				//printf("stack : %d Incomming %d StackTop : %s\n",operatiorPrecedence(stack[top]),operatiorPrecedence(ar[i]),stack);
+			}else{
+				while(stack[top] != '(' && top != -1 && operatiorPrecedence(ar[i]) <= operatiorPrecedence(stack[top])){
+					result[j++] = stack[top--];
+				}
+				stack[++top] = ar[i];
+			}
+		} 
+	}
+	while(top != -1){
+		result[j++] = stack[top--];
+	}	
+}
+
+int main(){
+	printf("Enter expression : ");
+	gets(expr);
+	printf("Infix expression : %s\n",expr);
+	convertInfixPostfix(expr);
+	printf("Postfix expression : %s\n",result);
+	return 0;
+}
